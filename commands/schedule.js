133386b13957962d3d3508f43a618b20caa76d76
Gabriel Cyr-St-Georges <gabriel.cyr-st-georges@polymtl.ca>
2021-07-20 17:26:24 -0400

    Schedule text save

--- commands/schedule.js
+++ commands/schedule.js
@@ -3,7 +3,15 @@ module.exports = {
     aliases: ['whenthefuckdoweraidagain'],
     description: 'Outputs the determined schedule.',
     execute(message, args) {
-        message.channel.send(
-            'CURRENT SCHEDULE\nTuesday 9h-11h30 PM EST\nWednesday 8h-11h PM EST\nThursday 9h-11h30 PM EST');
+        var fs = require('fs');
+        try {
+            const schedule = fs.readFileSync(`./schedules/${args[0]}.txt`, 'utf8');
+            message.channel.send(schedule);
+        } catch {
+            message.channel.send("Could not retrieve a file with that name!");
+        }
+
+        //message.channel.send(
+        //    'CURRENT SCHEDULE\nTuesday 9h-11h30 PM EST\nWednesday 8h-11h PM EST\nThursday 9h-11h30 PM EST');
     }
 }
